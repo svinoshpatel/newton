184aa79b5e9c336943e3f14801f83655a36c22cc
svinoshpatel <132487936+svinoshpatel@users.noreply.github.com>
2024-10-06 18:18:55 +0300

    Add files via upload

---
+++ nmdn/nmdn.dpr
@@ -0,0 +1,127 @@
+program nmdn;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+var
+  a, b, c, Fa, Fb, Fc, Eps, x, D, Dx : Real;
+  i, Kmax, count : Integer;
+  answer : String;
+
+Function f(x : Real) : Real;
+begin
+  // f := sqr(x-2) - 2
+  // f := x*x*x - 6*x*x + 11*x - 6
+  // f := 3*x - 4*ln(x) - 5
+  // f := -x*x +5
+  f := sqr(x-2)
+end;
+
+Function fp(x : Real) : Real;
+begin
+  D := Eps / 100;
+  fp := (f(x + D) - f(x)) / D;
+end;
+
+Function f2p(x : Real) : Real;
+begin
+  D := Eps / 100;
+  f2p := (f(x + D) + f(x - D) - 2*f(x)) / (D*D);
+end;
+
+begin
+
+  writeln('Choose solving algorithm');
+  writeln('[1] Newton''s method');
+  writeln('[2] Bisection method');
+  writeln('');
+  write('--> ');
+  readln(answer);
+
+  if answer = '1' then
+  begin
+    count := 0;
+    write('Input root interval, accuracy and max value of iterations: ');
+    readln(a, b, Eps, Kmax);
+
+    x := b;
+    if f(x) * f2p(x) < 0 then
+      x := a;
+    if f(x) * f2p(x) < 0 then
+    begin
+      writeln('For a given equation, the convergence of Newton''s method is not guaranteed');
+      readln;
+    end;
+    for i := 1 to Kmax do
+    begin
+      count := count + 1;
+      if fp(x) = 0 then
+      begin
+        writeln('Unable to calculate the derivative at x = ', x, ' Try different interval');
+        readln;
+        exit;
+      end;
+      Dx := f(x) / fp(x);
+      x := x - Dx;
+      if abs(Dx) <= Eps then
+      begin
+        writeln('Root: ', x:0:10);
+        writeln('Iterations: ', count);
+        readln;
+        exit;
+      end;
+    end;
+    writeln('For the given number of iterations, no root was found with Eps precision');
+    readln;
+  end
+  else if answer = '2' then
+  begin
+    count := 0;
+    write('Input root interval and accuracy: ');
+    readln(a, b, Eps);
+
+    repeat
+      Fa := f(a);
+      Fb := f(b);
+
+      if Abs(Fa) < Eps then
+      begin
+        c := a;
+        break;
+      end;
+
+      if Abs(Fb) < Eps then
+      begin
+        c := b;
+        break;
+      end;
+
+      if Fa * Fb < 0 then
+      begin
+        count := count + 1;
+        c := a + 0.5*(b - a);
+        Fc := f(c);
+        if Abs(Fc) < Eps then
+          break;
+        if Fa * Fc < 0 then
+          b := c
+        else
+          a := c
+      end
+      else
+      begin
+        writeln('Root is absent on the given interval');
+        readln;
+        exit
+      end
+    until Abs(b - a) < Eps;
+
+    writeln('Root: ', c:0:10);
+    write('Iterations: ', count);
+    readln;
+  end
+end.
